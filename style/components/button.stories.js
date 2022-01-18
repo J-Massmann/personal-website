6b904b583f277390e9c6b89c8854b5116c1171bb
Justus Massmann <justusmassmann@online.de>
2022-01-18 19:38:42 +0100

    Add storybook

---
+++ style/components/button.stories.js
@@ -0,0 +1,9 @@
+import "./button.css";
+
+export default {
+  title: "Components/Button",
+};
+
+export const Regular = () => `<button class="button">Test</button>`;
+
+export const Big = () => `<button class="button button--big">Test</button>`;
